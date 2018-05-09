309339626c51af082849847015a6acd90ea21294
commanderu <39104988+commanderu@users.noreply.github.com>
2018-05-09 04:20:46 +0300

    Add files via upload

---
+++ params.go
@@ -0,0 +1,10 @@
+// Copyright (c) 2013-2015 The btcsuite developers
+// Copyright (c) 2015 The commanderu developers
+// Use of this source code is governed by an ISC
+// license that can be found in the LICENSE file.
+
+package main
+
+import "github.com/commanderu/cdrwallet/netparams"
+
+var activeNet = &netparams.MainNetParams
